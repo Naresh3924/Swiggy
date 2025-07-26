1cc3fd29c15277b07c4fdf2a2f98299643bfe1eb
Naresh Krishna <nareshkrishna620@gmail.com>
2025-07-26 13:00:29 +0530

    shimmer update && firbase update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,18 +4,27 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-slate-100  border">
-      <div className="flex justify-center my-5">
+      <div className="flex justify-center border-b-2  py-5">
         <h1 className="font-bold text-2xl  py-5 ">
-          For better experience,download the Swiggy app now 
+          For better experience,download the Swiggy app now
         </h1>
-        <img className="w-72 h-20 px-5"
+        <img
+          className="w-72 h-20 px-5"
           src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/play_store.png"
           alt="social"
         />
-        <img className="w-60 h-20"
+        <img
+          className="w-60 h-20"
           src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/app_store.png"
           alt="social"
         />
+      </div>
+      <div className="flex  justify-between py-5 mx-48">
+        <h1 className="font-bold text-1xl">Company</h1>
+        <h1 className="font-bold text-1xl">Contact us</h1>
+        <h1 className="font-bold text-1xl">Legal</h1>
+        <h1 className="font-bold text-1xl">Life at Swiggy</h1>
+        <h1 className="font-bold text-1xl">Available in:</h1>
       </div>
     </div>
   );
