dba3a65956f7476ba2cf6ad15137dfba3ed446ba
Naresh Krishna <nareshkrishna620@gmail.com>
2025-08-07 21:06:11 +0530

    Update build folder  to deploy the application in netlify

--- src/components/cart/__tests__/CategoriesCard.spec.jsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import CategoriesCard from "./CategoriesCard";
-
-describe("TestCases for CategoriesCard componenent", () => {
-  test("should render CategoriesCard cmponent", () => {
-    render(<CategoriesCard />);
-  });
-});
