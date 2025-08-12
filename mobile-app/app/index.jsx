aa95db890d18565a69cdf3028562fcfce5880b07
Thibakar <59224006+Thibakar@users.noreply.github.com>
2025-08-12 22:09:10 +0530

    Mobile-Application|Introducing mobile application  using REACT-JS with EXPO-client

---
+++ mobile-app/app/index.jsx
@@ -0,0 +1,46 @@
+import { Alert, Button, ScrollView, StatusBar, StyleSheet, Text, TextInput, useColorScheme, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { useState } from 'react';
+
+
+export default function App() {
+  const [userName, setUserName] = useState('')
+
+  const handleUserName = (e) => {
+    setUserName(e?.target?.value)
+  }
+  const handleLogin=()=>{
+  }
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <StatusBar />
+        <ScrollView style={styles.container}>
+          <View>
+            <TextInput style={styles.input} value={userName} placeholder='Username' onChange={handleUserName} />
+            <Button title='Login' onPress={handleLogin} />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  input: {
+    borderWidth: 1,
+    marginTop: 12,
+    marginBottom: 12
+  },
+  button: {
+    marginTop: 12,
+    marginBottom: 12
+  }
+})
+
